1d0c4bfa837e463d9febfe566f0eef325c0ab7ad
r-salogubov <r.salogubov@seraphim.online>
2019-11-26 16:36:21 +0300

    wip

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by rsalogub on 26.11.19.
+//
+#include "vector2.h"
+
+int main()
+{
+    FRMath::Vector2 vector2(0,0);
+    return 0;
+}
---
+++ vector2.h
@@ -0,0 +1,16 @@
+//
+// Created by rsalogub on 26.11.19.
+//
+
+#ifndef VECTOR2_H
+#define VECTOR2_H
+
+#ifndef USE_FRMATH
+#include "realization/FRMathRealization/frvector2d.h"
+namespace FRMath
+{
+    using Vector2 = FRMathDef::FRVector2d;
+}
+#endif
+
+#endif //VECTOR2_H
